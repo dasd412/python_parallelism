18adf5747d5fcb6dc645a1b897de3fa285a11002
yang-yeongjun <dasd412@naver.com>
2023-11-29 16:55:36 +0900

    CPU Bound(1) - Synchronous

---
+++ section3/3_6.py
@@ -0,0 +1,48 @@
+import time
+
+"""
+
+Concurrency , Cpu Bound , I/O Bound - CPU Bound(1) - Synchronous
+
+keyword :CPU Bound
+
+"""
+
+
+# CPU-Bound 예제
+
+def cpu_bound(number):
+    return sum(i * i for i in range(number))
+
+
+def find_sums(numbers):
+    result = []
+
+    for number in numbers:
+        result.append(cpu_bound(number))
+
+    return result
+
+
+def main():
+    numbers = [3_000_000 + x for x in range(20)]
+    print(numbers)
+
+    start_time = time.time()
+
+    total = find_sums(numbers)
+
+    print()
+
+    print(f"Total list {total}")
+    print(f"Sum: {sum(total)}")
+
+    print()
+
+    print(f"Duration : {time.time() - start_time} seconds")
+
+    # Duration : 2.0953168869018555 seconds
+
+
+if __name__ == '__main__':
+    main()
